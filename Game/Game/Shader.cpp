dce4ed7fe46b67cf2b00a57c47c2f0ed7606abde
fca <fca>
2022-04-20 19:44:16 +0900

    少しやった

--- Game/Game/Shader.cpp
+++ Game/Game/Shader.cpp
@@ -12,3 +12,30 @@ bool Shader::Load(const string& _vertName, const string& _fragName)
 void Shader::SetActive()
 {
 }
+
+bool Shader::CompileShader(const string& _fileName, GLenum _shaderType, GLuint& _outShader)
+{
+	// ファイル読み込み
+	ifstream shaderFile(_fileName);
+	// ファイルを開く
+	if (shaderFile.is_open())
+	{
+		// すべてのテキストを1つの文字列に読み込む
+		stringstream sstream;
+		sstream << shaderFile.rdbuf();
+		string contents = sstream.str();
+		const char* contentsChar = contents.c_str();
+	}
+
+	return true;
+}
+
+bool Shader::IsCompiled(GLuint _shader)
+{
+	return false;
+}
+
+bool Shader::IsValidProgram()
+{
+	return false;
+}
--- Game/Game/Shader.h
+++ Game/Game/Shader.h
@@ -22,6 +22,31 @@ public:
 	// シェーダープログラムをアクティブに設定
 	void SetActive();
 
-	// @@@
-};
+private:
+	/// <summary>
+	/// シェーダーをコンパイルする
+	/// </summary>
+	/// <param name="_fileName">コンパイルするシェーダーのファイル名</param>
+	/// <param name="_shaderType">シェーダーの種類</param>
+	/// <param name="_outShader">シェーダーのIDを格納する参照変数</param>
+	/// <returns>true : コンパイル成功 , false : コンパイル失敗</returns>
+	bool CompileShader(const string& _fileName, GLenum _shaderType, GLuint& _outShader);
 
+	/// <summary>
+	/// シェーダーのコンパイルに成功したかの判定
+	/// </summary>
+	/// <param name="_shader">シェーダのID</param>
+	/// <returns>true : 成功 , false : 失敗</returns>
+	bool IsCompiled(GLuint _shader);
+
+	// 頂点/フラグメントプログラムのリンクを確認
+	bool IsValidProgram();
+
+	/* シェーダーオブジェクトのIDを格納 */
+	// 頂点シェーダー
+	GLuint mVertexShader;
+	// フラグメントシェーダー
+	GLuint mFragShader;
+	// シェーダープログラム（頂点シェーダーとフラグメントシェーダーを結合）
+	GLuint mShaderProgram;
+};
